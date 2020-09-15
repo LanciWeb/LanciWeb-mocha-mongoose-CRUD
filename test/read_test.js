e9807d82a3551f01dbf359839f2f80f2517ea1be
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-15 17:23:45 +0200

    added find all user test

---
+++ test/read_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users', () => {
+  // We are manually adding this user here because in the main mocha file (test_helper) we are running a
+  let joe;
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save().then(() => done());
+  });
+  it('read all users', (done) => {
+    User.find({ name: 'Joe' }).then((users) => {
+      console.log(users);
+      assert(users[0]._id.toString() === joe._id.toString());
+      done();
+    });
+  });
+});
