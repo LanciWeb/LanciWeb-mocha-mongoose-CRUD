01d7da832e28fcfa62d203f585855833d0488b1b
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-17 12:49:17 +0200

    added custom validation with function and test name length

--- src/user.js
+++ src/user.js
@@ -3,10 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: { type: String, required: [true, 'Name is required.'] },
+  name: {
+    type: String,
+    required: [true, 'Name is required.'],
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters',
+    },
+  },
   postCount: Number,
 });
 
 const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
+
+//#VALIDATION
+/**
+ * we can perform validation in different ways.
+ * When we define a property, instead of specifying only the type we could pass an object, where explicitly declare the type and some other properties that could be used by mongo as validation. For example the 'required' flag.
+ *
+ * as an alternative, we can use a 'validator' property to set a custom function and a message to be displayed in case of validation failure. The function receives the model property as its first argument, then we can operate with it.
+ */
--- test/validation_test.js
+++ test/validation_test.js
@@ -2,11 +2,19 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('validtation tests', () => {
-  it('Requires and validate a user name', (done) => {
+  it('Requires a user name', (done) => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
     assert(message === 'Name is required.');
+    done();
+  });
+
+  it('Validates user name length', (done) => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === 'Name must be longer than 2 characters');
     done();
   });
 });
