831699aeaa331714e0573676f881319d7ea35534
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-18 11:03:02 +0200

    added association of models

---
+++ test/associations_test.js
@@ -0,0 +1,47 @@
+const mongoose = require('Mongoose');
+const User = require('../src/models/user');
+const Comment = require('../src/models/comment');
+const BlogPost = require('../src/models/blogPost');
+
+describe('Associations tests', () => {
+  let joe, blogPost, comment;
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({
+      title: 'JS is Great',
+      content: 'Yep it really is',
+    });
+    comment = new Comment({ content: 'Congrats on great post.' });
+
+    //#has Many Relationship assignation
+    joe.blogPost.push(blogPost);
+    blogPost.comments.push(comment);
+
+    //# has one Relationship assignation
+    comment.user = joe;
+
+    joe.save();
+    comment.save();
+    blogPost.save();
+
+    Promise.all([joe.save(), comment.save(), blogPost.save()]).then(() =>
+      done()
+    );
+  });
+});
+
+//#has Many Relationship assignation
+/**
+ * even though this is not a subdocument, we create the relationship in the same fashion: by pushing a model instance into the property of the relationship owner as it were a simple array.
+ * ! we need to save after the assignation
+ *
+ * ? Even though the schema expects an array of object Id as a type, mongoose takes care of that!
+ */
+
+//# has one Relationship assignation
+/**
+ * Also in this case, we can treat the association as it was a simple property, that is, by directly assigning the instance we want to tie to the owner of the relationship.
+ * ! we need to save after the assignation
+ *
+ * ? Even though the schema expects an object Id as a type, mongoose takes care of that!
+ */
