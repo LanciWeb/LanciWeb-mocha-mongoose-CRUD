bf237466d88a65509591d06eb5ee15a2b84271c5
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-18 16:34:30 +0200

    added middleware test

---
+++ test/middleware_test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const mongoose = require('mongoose');
+const User = require('../src/models/user');
+const BlogPost = require('../src/models/blogPost');
+describe('middleware', () => {
+  let joe, blogPost;
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({
+      title: 'JS is Great',
+      content: 'Yep it really is',
+    });
+
+    //#has Many Relationship assignation
+    joe.blogPosts.push(blogPost);
+
+    //# has one Relationship assignation
+
+    Promise.all([joe.save(), blogPost.save()]).then(() => done());
+  });
+
+  it.only('when user is deleted also blogPosts are removed', (done) => {
+    joe
+      .remove()
+      .then(() => BlogPost.countDocuments())
+      .then((count) => {
+        console.log(count);
+        assert(count === 0);
+        done();
+      });
+  });
+});
